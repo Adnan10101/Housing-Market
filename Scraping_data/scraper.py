a9c60b5ce9976a25fafe01ce33f1130f008e7c03
Adnan <mohammedadnan10101@gmail.com>
2022-07-25 13:55:48 +0530

    updating scraper

--- Scraping_data/scraper.py
+++ Scraping_data/scraper.py
@@ -1,12 +1,8 @@
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-from selenium.webdriver import ChromeOptions
-
 import csv
 import numpy as np
-import re
+
+from bs4 import BeautifulSoup
 
 
 
@@ -18,14 +14,13 @@ with open("house_details.csv","w") as file:
 def scrapper(names):
 
     for name in names:
-       
-
+        
         for i in range(1,6):
-            print(name)
-            print(i)
+            #print(name)
+            #print(i)
             page = name+"&page={}".format(i)
             request = requests.get(page)
-            soup = BeautifulSoup(request.content,"lxml")
+            soup = BeautifulSoup(request.content,"html.parser")
             with open("house_details.csv","a") as file:
                 w = csv.writer(file)
                 
@@ -40,7 +35,11 @@ def scrapper(names):
                     area = area_td_class.find("span",class_ = "val").text
                     
                     bed_div = head.find("div",class_ = "title-line")
-                    bed = bed_div.find("span",class_ = "val").text
+                    bed = bed_div.find("span",class_ = "val")
+                    if bed:
+                        bed = bed.text
+                    else:
+                        bed = np.nan
                     
                     bath_ul = head.find("ul",class_ = "listing-details")
                     bath = bath_ul.find("li",title = "Bathrooms")
@@ -61,72 +60,33 @@ def scrapper(names):
                     
                     w.writerow([tot_price,area,bed,bath,location,builder,status])
 
-def get_url(source):
-    url = re.findall('https://www.makaan.com/price-trends/property-rates-for-buy-in-chennai')
-    reqs = requests.get(url)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
-    urls = []
-    for link in soup.find_all('a'):
-        print(link.get('href'))
 
     
+def get_url(link):
+    request = requests.get(link)
+    main_url = "https://www.makaan.com"
+    
+    soup = BeautifulSoup(request.text,"html.parser")
+    all_urls = []
+    bb = soup.find("div",class_ = "tbl-wrap") 
+    
+    for url in bb.find_all("tr", itemtype = "http://schema.org/Place"):
+        get_url = url.find_all(attrs = {"data-url":True})
+        if get_url:
+            get_url = main_url + get_url[0]["data-url"]
+            all_urls .append(get_url)
+    
+    return all_urls
 
 
 source = "https://www.makaan.com/price-trends/property-rates-for-buy-in-chennai"
 
-
-# Selenium driver
-
-opts = ChromeOptions()
-opts.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome("D:/chromedriver.exe",chrome_options=opts)
-
-
-locations_name = ["https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50420&localityName=avadi&suburbId=10015&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50307&localityName=sholinganallur&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51578&localityName=perungalathur&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50288&localityName=medavakkam&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51578&localityName=perungalathur&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51578&localityName=perungalathur&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50346&localityName=porur&suburbId=10015&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50403&localityName=kelambakkam&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51276&localityName=chengalpattu&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50300&localityName=poonamallee&suburbId=10015&listingType=buy&propertyType=apartment"
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50314&localityName=mogappair&suburbId=10015&listingType=buy&propertyType=apartment"
-
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=52047&localityName=selaiyur&suburbId=10016&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51142&localityName=madipakkam&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50352&localityName=navallur&suburbId=10257&suburbName=omr&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50315&localityName=kolathur&suburbId=10013&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50312&localityName=urapakkam&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51076&localityName=ambattur&suburbId=10015&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51627&localityName=perungudi&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51532&localityName=siruseri&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50349&localityName=pallavaram&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51229&localityName=west-tambaram&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=52316&localityName=padur&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50306&localityName=velachery&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51151&localityName=perumbakkam&suburbId=10016&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50286&localityName=chromepet&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=50310&localityName=pallikaranai&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=52276&localityName=neelankarai&suburbId=10259&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51668&localityName=madambakkam&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51571&localityName=singaperumal-koil&suburbId=10263&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51280&localityName=thoraipakkam-omr&suburbId=10257&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51618&localityName=t-nagar&suburbId=10202&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51697&localityName=choolaimedu&suburbId=10202&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=51248&localityName=adyar&suburbId=10202&listingType=buy&propertyType=apartment",
-"https://www.makaan.com/listings?cityId=5&cityName=chennai&localityId=52056&localityName=karapakkam&suburbId=10257&listingType=buy&propertyType=apartment"
+urls = get_url(source)
+scrapper(urls)
 
 
 
 
-
-
-
-]
-scrapper(locations_name)
 
 
 
